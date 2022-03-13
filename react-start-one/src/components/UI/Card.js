c093de65f0045907cc13cfe62048a6448a9c269a
burakozcelik5028 <burakozcelik5028@hotmail.com>
2022-03-13 23:18:11 +0300

    update

--- react-start-one/src/components/UI/Card.js
+++ react-start-one/src/components/UI/Card.js
@@ -4,7 +4,7 @@ const Card = (props) => {
     const classes = 'card ' + props.className;
     return (
         
-        <div className={classes }>
+        <div className={classes}>
             {props.children}
         </div>
     );

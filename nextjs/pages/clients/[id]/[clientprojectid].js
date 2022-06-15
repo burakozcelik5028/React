d4098f41d0a159f57a5e78bb38ddcd1384942830
BurakOzcelik <burak@kreo.solutions>
2022-06-15 08:43:46 +0300

    404 error message added

--- nextjs/pages/clients/[id]/[clientprojectid].js
+++ nextjs/pages/clients/[id]/[clientprojectid].js
@@ -4,7 +4,11 @@ function SelectedClientProjectPage(){
     const router = useRouter();
 
     return <div>
-        <h1>The selected project with the Id = {router.query.clientprojectid} of selected client with the id = {router.query.id}</h1>
+        <h1>The selected project with the Id = 
+            {router.query.clientprojectid} 
+            of selected client with the id = 
+            {router.query.id}
+        </h1>
     </div>
 }
 

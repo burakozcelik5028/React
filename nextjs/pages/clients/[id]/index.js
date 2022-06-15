c39286152c281208485a094387827818a1506ee0
BurakOzcelik <burak@kreo.solutions>
2022-06-15 08:42:46 +0300

    pathname and query added to Link and next/router

--- nextjs/pages/clients/[id]/index.js
+++ nextjs/pages/clients/[id]/index.js
@@ -3,8 +3,16 @@ import {useRouter} from 'next/router';
 function ClientProjectsPage(){
     const router = useRouter();
     
+    const loadProjectsHandler = () =>{
+        router.push({
+            pathname: "/clients/[id]/[clientprojectid]",
+            query: {id: "burak", clientprojectid: "28"}
+        })
+    }
+
     return <div>
         <h1>The Projects Of Selected Client with id = {router.query.id}</h1>
+        <button onClick={loadProjectsHandler}>Load Project A</button>
     </div>
 }
 
--- nextjs/pages/clients/index.js
+++ nextjs/pages/clients/index.js
@@ -4,12 +4,16 @@ function ClientPage(){
 
     const clients = [{id: '28', name: 'Burak'},{id: '70', name: 'Nuri'},{id: '67', name: 'Latif'}]
 
-    return <div>
+    return <div>    
         <h1>The Client Page</h1>
         <ul>
             {clients.map((client) =>(
                 <li key={client.id}>
-                    <Link href={`/clients/${client.id}`}>{client.name}</Link>
+                    {/* <Link href={`/clients/${client.id}`}>{client.name}</Link> */}
+                    <Link href={{
+                        pathname: "/clients/[id]",
+                        query: {id: client.id},
+                    }}>{client.name}</Link>
                 </li>
             ))}
         </ul>

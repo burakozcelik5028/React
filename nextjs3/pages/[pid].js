2ab9bb23108ddabacae0494acefa25889ea7c42b
BurakOzcelik <burak@kreo.solutions>
2022-06-23 14:42:41 +0300

    falback: false- loading control is not workin- automatic notFound page is opening

--- nextjs3/pages/[pid].js
+++ nextjs3/pages/[pid].js
@@ -4,6 +4,16 @@ import path from "path";
 
 function ProductDetailPage(props) {
   const { loadedProduct } = props;
+
+  if (!loadedProduct) {
+    //if me make fallback: "blocking" in getStaticPaths, we don't need this control here.
+    return (
+      <Fragment>
+        <h1>Loading...</h1>
+      </Fragment>
+    );
+  }
+
   return (
     <Fragment>
       <h1>{loadedProduct.title}</h1>
@@ -12,23 +22,38 @@ function ProductDetailPage(props) {
   );
 }
 
-export async function getStaticProps(context) {
-  const { params } = context;
-  const productID = params.pid;
-
+async function getData() {
   const filePath = path.join(process.cwd(), "data", "dummy-backend.json");
   const jsonData = await fs.readFile(filePath, "utf8");
   const data = JSON.parse(jsonData);
 
+  return data;
+}
+
+export async function getStaticProps(context) {
+  const { params } = context;
+  const productID = params.pid;
+
+  const data = await getData();
   const product = data.products.find((product) => product.id === productID);
+
+  if (!product) {
+    //if the fallback: true in getStaticPaths, it will get error if there is no data. than we should use this control
+    return { notFound: true };
+  }
 
   return { props: { loadedProduct: product } };
 }
 
 export async function getStaticPaths(pid) {
+  const data = await getData();
+  const ids = data.products.map((product) => product.id);
+  //I am creating all possible ID pages before opening the page. that is why I can set fallback: false
+  const pathWithParams = ids.map((id) => ({ params: { pid: id } }));
+
   return {
-    paths: [], //indicates that no page needs be created at build time
-    fallback: "blocking", //indicates the type of fallback
+    paths: pathWithParams, //indicates that no page needs be created at build time
+    fallback: false, //indicates the type of fallback you can put "blocking" as a value than you dont need to check !loadedProduct for Loading
   };
 }
 
